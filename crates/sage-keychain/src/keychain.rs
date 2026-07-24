572ef7158e1a3bdc3c168720de931d42fc6d896f
MrDennis <jesus.dennis@hotmail.com>
2026-07-24 13:50:30 -0700

    make sage-keychain wasm-compatible by depending on chia-bls directly

--- crates/sage-keychain/src/keychain.rs
+++ crates/sage-keychain/src/keychain.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use bip39::Mnemonic;
-use chia_wallet_sdk::prelude::*;
+use chia_bls::{PublicKey, SecretKey};
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
 
